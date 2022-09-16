17110bc3fb2400c8f54c7b708a106360dbfa552b
Quard273 <xspokinokix@yandex.ru>
2022-09-16 16:50:47 +0300

    Сделал лабу

--- test/test_CalcRating.py
+++ test/test_CalcRating.py
@@ -3,7 +3,7 @@ from src.Types import DataType
 from src.CalcRating import CalcRating
 import pytest
 
-RatingsType = dict[str, float]
+RatingsType = int
 
 
 class TestCalcRating:
@@ -13,24 +13,21 @@ class TestCalcRating:
         data: DataType = {
             "Абрамов Петр Сергеевич":
                 [
-                    ("математика", 80),
-                    ("русский язык", 76),
-                    ("программирование", 100)
+                    ("математика", 20),
+                    ("русский язык", 35),
+                    ("программирование", 91)
                 ],
 
             "Петров Игорь Владимирович":
                 [
-                    ("математика", 61),
-                    ("русский язык", 80),
-                    ("программирование", 78),
+                    ("математика", 92),
+                    ("русский язык", 92),
+                    ("программирование", 92),
                     ("литература", 97)
                 ]
         }
 
-        rating_scores: RatingsType = {
-            "Абрамов Петр Сергеевич": 85.3333,
-            "Петров Игорь Владимирович": 79.0000
-        }
+        rating_scores: RatingsType = 1
 
         return data, rating_scores
 
@@ -43,7 +40,4 @@ class TestCalcRating:
     def test_calc(self, input_data: tuple[DataType, RatingsType]) -> None:
 
         rating = CalcRating(input_data[0]).calc()
-        for student in rating.keys():
-            rating_score = rating[student]
-            assert pytest.approx(rating_score,
-                                 abs=0.001) == input_data[1][student]
+        assert rating.countStudent==input_data[1]
