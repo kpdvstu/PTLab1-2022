33815c630ba9c911d2afdb4b934e75bf15b6e25e
Nikitin Denis Valerievich <Nikden_72@mail.ru.com>
2022-09-16 14:57:54 +0300

    simple comment

--- src/main.py
+++ src/main.py
@@ -19,8 +19,9 @@ def main():
 
     reader = TextDataReader()
     students = reader.read(path)
+    #вывод студента
     print("Students: ", students)
-
+    #вывод рейтинга
     rating = CalcRating(students).calc()
     print("Rating: ", rating)
 
