b1623a7001a17a57697d9d18eccf7c10f9db66d8
alavirttual <kufnikov.14@gmail.com>
2022-09-25 21:56:23 +0300

    ver s

--- src/main.py
+++ src/main.py
@@ -6,23 +6,25 @@ from CalcRating import CalcRating
 from TextDataReader import TextDataReader
 
 
-def get_path_from_arguments(args) -> str:
+def get_path_from_arguments() -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
     parser.add_argument("-p", dest="path", type=str, required=True,
                         help="Path to datafile")
-    args = parser.parse_args(args)
+    args = parser.parse_args()
     return args.path
 
 
 def main():
-    path = get_path_from_arguments(sys.argv[1:])
+    path = get_path_from_arguments() # Открываем файл и записываем содержимое в path
 
-    reader = TextDataReader()
-    students = reader.read(path)
-    print("Hallo")  # testt
-    print("Students: ", students)
-    rating = CalcRating(students).calc()
-    print("Rating: ", rating)
+    reader = TextDataReader()        # Конструктор класса TextDataReader
+    students = reader.read(path)     # Записываем в student словарь построенный на основе path
+    print("Students: ", students)    # Выводим в консоль получившийся словарь
+    rating = CalcRating(students).calc() # Содаём конструктор класса CalcRating
+                                         # передаём ему словарь students
+                                         # в функции calc считаем срений бал
+
+    print("Rating: ", rating)            # Печатаем словарь с рейтингом студентов
 
 
 if __name__ == "__main__":
