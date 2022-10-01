c4e46467ade50fca528cb35b36f24f40a7e72aab
alavirttual <kufnikov.14@gmail.com>
2022-10-01 21:07:18 +0300

    add class readjson

--- src/TextDataReader.py
+++ src/TextDataReader.py
@@ -20,3 +20,28 @@ class TextDataReader(DataReader):
                     self.students[self.key].append(
                         (subj.strip(), int(score.strip())))
         return self.students
+
+class JsonDataReader(DataReader):
+
+    def __init__(self):
+        self.key: str = ""
+        self.students: DataType = {}
+
+    def read(self, path: str) -> DataType:
+        with open(path, encoding='utf-8') as file:
+            for line in file:
+                if line.endswith(": {'):
+                    self.key = line.strip("\" : {")
+                    self.students[self.key] = []
+                    print(self.key)
+                elif not line.startswith("{ }  "):
+                    subj, score = line.split(": ", maxsplit=1)
+                    subj = subj.strip("\"")
+                    score = score.strip(",")
+                    self.students[self.key].append(
+                        (subj.strip(), int(score.strip())))
+        return self.students
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import sys
 
 from CalcRating import CalcRating
 from TextDataReader import TextDataReader
+from TextDataReader import JsonDataReader
 
 
 def get_path_from_arguments(args) -> str:
@@ -11,21 +12,25 @@ def get_path_from_arguments(args) -> str:
     parser.add_argument("-p", dest="path", type=str, required=True,
                         help="Path to datafile")
     args = parser.parse_args(args)
+
     return args.path
 
 
 def main():
     path = get_path_from_arguments(sys.argv[1:]) # Открываем файл и записываем содержимое в path
 
-    reader = TextDataReader()        # Конструктор класса TextDataReader
+#    reader = TextDataReader()        # Конструктор класса TextDataReader
+    reader = JsonDataReader()
     students = reader.read(path)     # Записываем в student словарь построенный на основе path
+
+
     print("Students: ", students)    # Выводим в консоль получившийся словарь
-    rating = CalcRating(students).calc() # Содаём конструктор класса CalcRating
+#    rating = CalcRating(students).calc() # Содаём конструктор класса CalcRating
                                          # передаём ему словарь students
                                          # в функции calc считаем срений бал
 
-    print("Rating: ", rating)            # Печатаем словарь с рейтингом студентов
-    print("new_branch")
+ #   print("Rating: ", rating)            # Печатаем словарь с рейтингом студентов
+
 
 
 if __name__ == "__main__":
