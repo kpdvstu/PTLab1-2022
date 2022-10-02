5f1c88441fd87bfd19d345e7eb7b52bd7c9dfb97
alavirttual <kufnikov.14@gmail.com>
2022-10-02 18:06:51 +0300

    add test JsonDataReader

--- src/main.py
+++ src/main.py
@@ -17,20 +17,14 @@ def get_path_from_arguments(args) -> str:
 
 
 def main():
-    path = get_path_from_arguments(sys.argv[1:]) # Открываем файл и записываем содержимое в path
-
-#    reader = TextDataReader()        # Конструктор класса TextDataReader
+    path = get_path_from_arguments(sys.argv[1:])
+#    reader = TextDataReader()
     reader = JsonDataReader()
-    students = reader.read(path)     # Записываем в student словарь построенный на основе path
+    students = reader.read(path)
+    print("Students: ", students)
 
-
-    print("Students: ", students)    # Выводим в консоль получившийся словарь
-#    rating = CalcRating(students).calc() # Содаём конструктор класса CalcRating
-                                         # передаём ему словарь students
-                                         # в функции calc считаем срений бал
-
- #   print("Rating: ", rating)            # Печатаем словарь с рейтингом студентов
-
+#    rating = CalcRating(students).calc()
+#    print("Rating: ", rating)
 
 
 if __name__ == "__main__":
