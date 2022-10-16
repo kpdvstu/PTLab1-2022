5337ccdb4f687732bdf8af20bc80a349c50ab760
alavirttual <kufnikov.14@gmail.com>
2022-10-16 20:03:25 +0300

    fix_syntax

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ def get_path_from_arguments(args) -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
     parser.add_argument("-p", dest="path", type=str, required=True,
                         help="Path to datafile")
-
     args = parser.parse_args(args)
     return args.path
 
